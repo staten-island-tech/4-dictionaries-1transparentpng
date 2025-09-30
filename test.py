2d5466b2b5eb53336abe4899e8162e09d453adbd
1transparentpng <aidens151@nycstudents.net>
2025-09-30 09:24:17 -0400

    microcenter

---
+++ test.py
@@ -0,0 +1,12 @@
+catalog = [
+    {
+        "itemname": "AMD Ryzen 7 7700X",
+        "price": float(239.99),
+        "description": "8-Core, 16 Threads, Unlocked Desktop Processor"
+    },
+    {
+        "itemname": "",
+        "price": float(69.67),
+        "description": "hmmm... idk"
+    }
+]
